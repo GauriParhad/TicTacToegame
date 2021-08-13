f197b91293b8d1b2d3a506e45421c567f649d8bf
Gaurip10 <thegauriparhad@gmail.com>
2021-08-13 08:17:33 +0530

    Added: Giving inputs

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,9 +1,11 @@
 public class TicTacToeGame {
+    import java.util.Scanner;
+
 
     //creating empty board
     public static char[] createboard() {
         char[] board = new char[10];
-        for (int i = 1; i < board.length; i++) {     //Ignoring 0th// index
+        for (int i = 1; i < board.length; i++) {     //Ignoring 0th index
             board[i] = ' ';
         }
         return board;
@@ -13,5 +15,28 @@ public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToeGame");
         createboard();
+    }
+
+    //Creating method for playercomputer
+    //Giving the inputs X or O
+
+    public static void playercomputer() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Two choices to play");
+        System.out.println("O: Play");
+        System.out.println("X: No Play");
+        System.out.println("Enter the choice O or X");
+        char ch = scan.next().charAt(0);
+        switch (ch) {
+            case 'O':
+                System.out.println("congratulations ypu can play the game.");
+                break;
+            case 'O':
+                System.out.println("Sorry, you cannot play the game.");
+                break;
+            default:
+                System.out.println("Invalid input");
+                break;
+        }
     }
 }
